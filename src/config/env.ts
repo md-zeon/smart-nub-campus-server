cebe326e620ed8d494f48be74a9fa83727d10fcc
md-zeon <zeon.cse@gmail.com>
2026-07-08 08:47:13 +0600

    feat: integrate better-auth with PostgreSQL support and update environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,8 +8,8 @@ interface EnvConfig {
   NODE_ENV: string;
   PORT: string;
   DATABASE_URL: string;
-  //   BETTER_AUTH_SECRET: string;
-  //   BETTER_AUTH_URL: string;
+  BETTER_AUTH_SECRET: string;
+  BETTER_AUTH_URL: string;
   //   BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string;
   //   BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string;
   //   ACCESS_TOKEN_SECRET: string;
@@ -23,8 +23,8 @@ const loadEnvVariables = (): EnvConfig => {
     "NODE_ENV",
     "PORT",
     "DATABASE_URL",
-    // "BETTER_AUTH_SECRET",
-    // "BETTER_AUTH_URL",
+    "BETTER_AUTH_SECRET",
+    "BETTER_AUTH_URL",
     // "BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN",
     // "BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE",
     // "ACCESS_TOKEN_SECRET",
@@ -47,8 +47,8 @@ const loadEnvVariables = (): EnvConfig => {
     NODE_ENV: process.env.NODE_ENV as string,
     PORT: process.env.PORT as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
-    // BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
-    // BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
     // BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env
     //   .BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
     // BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env
