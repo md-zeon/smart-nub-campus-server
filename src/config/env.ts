69e86289db18638df50e1674c06b692182ab6407
md-zeon <zeon.cse@gmail.com>
2026-07-08 07:40:42 +0600

    feat: migrate to ES modules and add Prisma setup
    - Changed package type to module in package.json
    - Added Prisma dependencies and devDependencies
    - Created prisma.config.ts for Prisma configuration
    - Added schema.prisma for database schema definition
    - Implemented environment variable loading in src/config/env.ts
    - Updated server.ts to use environment variables for configuration
    - Modified tsconfig.json for ESNext module support and adjusted include/exclude settings

---
+++ src/config/env.ts
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+// import AppError from "../app/errorHelpers/AppError";
+// import status from "http-status";
+
+dotenv.config();
+
+interface EnvConfig {
+  NODE_ENV: string;
+  PORT: string;
+  DATABASE_URL: string;
+  //   BETTER_AUTH_SECRET: string;
+  //   BETTER_AUTH_URL: string;
+  //   BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string;
+  //   BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string;
+  //   ACCESS_TOKEN_SECRET: string;
+  //   ACCESS_TOKEN_EXPIRES_IN: string;
+  //   REFRESH_TOKEN_SECRET: string;
+  //   REFRESH_TOKEN_EXPIRES_IN: string;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables = [
+    "NODE_ENV",
+    "PORT",
+    "DATABASE_URL",
+    // "BETTER_AUTH_SECRET",
+    // "BETTER_AUTH_URL",
+    // "BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN",
+    // "BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE",
+    // "ACCESS_TOKEN_SECRET",
+    // "ACCESS_TOKEN_EXPIRES_IN",
+    // "REFRESH_TOKEN_SECRET",
+    // "REFRESH_TOKEN_EXPIRES_IN",
+  ];
+  for (const variable of requiredEnvVariables) {
+    if (!process.env[variable]) {
+      //   throw new AppError(
+      //     status.INTERNAL_SERVER_ERROR,
+      //     `Environment variable ${variable} is required but not defined in .env file`,
+      //   );
+      throw new Error(
+        `Environment variable ${variable} is required but not defined in .env file`,
+      );
+    }
+  }
+  return {
+    NODE_ENV: process.env.NODE_ENV as string,
+    PORT: process.env.PORT as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
+    // BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+    // BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+    // BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env
+    //   .BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
+    // BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env
+    //   .BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string,
+    // ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+    // ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+    // REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+    // REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+  };
+};
+
+const ENVVARS = loadEnvVariables();
+
+export default ENVVARS;
